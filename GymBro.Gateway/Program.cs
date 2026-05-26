25fc360d6558088e33cdfadc50fb2d198aa0054b
VUTRUONGTHANH <vutruongthanh25k@gmail.com>
2026-05-26 21:59:36 +0700

    cổng port chung

---
+++ GymBro.Gateway/Program.cs
@@ -0,0 +1,17 @@
+﻿using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Thêm file cấu hình ocelot.json vào hệ thống cấu hình của ứng dụng
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+// Đăng ký các dịch vụ của Ocelot vào DI Container
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+// Kích hoạt Ocelot Middleware ở cuối quy trình xử lý request
+await app.UseOcelot();
+
+app.Run();
