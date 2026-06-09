c0673dfdf6f510671338d6f985371aad5013d9c0
VUTRUONGTHANH <vutruongthanh25k@gmail.com>
2026-06-09 16:19:13 +0700

    NNKH 1.0

--- GymBro.Order.API/Program.cs
+++ GymBro.Order.API/Program.cs
@@ -46,6 +46,7 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<OrderDbContext>();
+    db.Database.Migrate();
     if (!await db.PaymentMethods.AnyAsync())
     {
         db.PaymentMethods.AddRange(
